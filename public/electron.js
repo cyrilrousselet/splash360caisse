a4f2ce23753f0783b036100b3e0859864062f315
Cyril Rousselet <cyril@aqua-forte.net>
2019-12-11 22:31:59 +0100

    devtool darwin

--- public/electron.js
+++ public/electron.js
@@ -16,23 +16,14 @@ function createWindow() {
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 //    if (isDev) {
        //  Open the DevTools.
+       if (process.platform === 'darwin') {
          BrowserWindow.addDevToolsExtension(
              path.join(os.homedir(), '/Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.17.0_0')
          );
+       }
         mainWindow.webContents.openDevTools();
 //    }
     mainWindow.on('closed', () => mainWindow = null);
-
-    // mainWindow.once('ready-to-show', () => {
-    //     electron.protocol.interceptFileProtocol('file', (request, callback) => {
-    //         const filePath = request.url.replace('file://', '');
-    //         const url = request.url.includes('static/media/') ? path.normalize(`${__dirname}/${filePath}`) : filePath;
-    
-    //         callback({ path: url });
-    //     }, err => {
-    //         if (err) console.error('Failed to register protocol');
-    //     });
-    // });
 
 }
 
