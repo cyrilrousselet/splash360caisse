4474bed8f4591b41622b81fea0f8772ab046ecc7
Cyril Rousselet <cyril@aqua-forte.net>
2020-03-06 16:22:25 +0100

    CORS : origin '*'

--- public/server.js
+++ public/server.js
@@ -4,30 +4,30 @@ const cors = require('cors');
 
 
 const allowedOrigins = [
-  // 'http://127.0.0.1',
-  // 'http://localhost',
-  // 'http://192.168.1.185'
-  '*'
+  'http://127.0.0.1',
+  'http://localhost',
+  'http://192.168.1.185'
+  //'*'
 ];
 
 const server = {
   init: () => {
     const xpr = express();
 
-    xpr.use(cors({
-      origin: function(origin, callback){
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-          var msg = 'The CORS policy for this site does not ' +
-                    'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      }
-    }));
-
+    // xpr.use(cors({
+    //   origin: function(origin, callback){
+    //     // allow requests with no origin
+    //     // (like mobile apps or curl requests)
+    //     if(!origin) return callback(null, true);
+    //     if(allowedOrigins.indexOf(origin) === -1){
+    //       var msg = 'The CORS policy for this site does not ' +
+    //                 'allow access from the specified Origin.';
+    //       return callback(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    //   }
+    // }));
+    xpr.use(cors({origin: "*"}));
 
     xpr.use(express.urlencoded({extended: false})).use(express.json());
     xpr.get('/', (req,res) => {
