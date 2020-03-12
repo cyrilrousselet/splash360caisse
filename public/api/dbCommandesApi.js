f33bdbffa400ff3d3602199cc60d99ecff5c20f8
Cyril Rousselet <cyril@aqua-forte.net>
2020-03-12 11:54:29 +0100

    correction base ChickenStreet

--- public/api/dbCommandesApi.js
+++ public/api/dbCommandesApi.js
@@ -1,6 +1,6 @@
 const db = require('../db.js');
 const log = require('electron-log');
-const hydration = require('../dev/dbhydration.js');
+const hydration = require('../dev/dbhydration_chickenstreet.js');
 const {commandes} = hydration;
 
 
