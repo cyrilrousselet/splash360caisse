c662fbe9bc5863d579257e252ca33abe88c9466a
Cyril Rousselet <cyril@aqua-forte.net>
2021-02-07 00:23:23 +0100

    corr. send bulksync

--- public/api/server.js
+++ public/api/server.js
@@ -734,7 +734,7 @@ function bulkSyncToPrimary(db, data) {
   __request.setHeader("Access-Control-Allow-Origin", "*");
   __request.setHeader("Content-Type", "application/json");
 
-  __request.write(JSON.stringify({ db, data, _emitter }));
+  __request.write(JSON.stringify({ db, data, emitter:_emitter }));
 
   __request.on("response", (response) => {
     log.info(
